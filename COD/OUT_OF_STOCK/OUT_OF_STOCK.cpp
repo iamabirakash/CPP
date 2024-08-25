7b5ff3eba798e16f71e4cbec5f869d65406c54b4
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2024-08-25 22:07:07 +0530

    Aug 25

---
+++ COD/OUT_OF_STOCK/OUT_OF_STOCK.cpp
@@ -0,0 +1,17 @@
+// You are using GCC
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    int arr[a];
+    for(int i=0;i<a;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<a;i++){
+        if(arr[i]!=0){
+            cout<<arr[i]<<" ";
+        }
+    }
+    
+}
