363980d497a555a2ec1824f4b944ff26dfdaa1a5
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2024-08-27 12:01:29 +0530

    Added another format

--- Class/STATIC_MEMBER_FUNCTION/STATIC_MEMBER_FUNCTION.cpp
+++ Class/STATIC_MEMBER_FUNCTION/STATIC_MEMBER_FUNCTION.cpp
@@ -25,3 +25,33 @@ int main(){
     }
     
 }
+
+
+// #include<iostream>
+// using namespace std;
+// class admin{
+//     int roll;
+//     double marks;
+//     char name[20];
+//     static int regno;
+//     public:
+//         void get(){
+//             cin>>name>>roll>>marks;
+//             regno++;
+//         }
+//         void put(){
+//             get();
+//             cout<<name<<" "<<" "<<roll<<marks<<" "<<regno<<endl;
+//         }
+// };
+// int admin::regno = 2000;
+// int main(){
+//     admin a[3];
+//     // for(int i=0;i<3;i++){
+//     //     a[i].get();
+//     // }
+//     for(int i=0;i<3;i++){
+//         a[i].put();
+//     }
+    
+// }
