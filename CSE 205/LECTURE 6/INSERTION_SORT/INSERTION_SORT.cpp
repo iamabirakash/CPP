f58b85c607c405088931d7f2224166dbc49d3210
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2024-09-01 15:30:55 +0530

    Sep 1

---
+++ CSE 205/LECTURE 6/INSERTION_SORT/INSERTION_SORT.cpp
@@ -0,0 +1,26 @@
+// You are using GCC
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    for(int i=1;i<n;i++){
+        int key=a[i];
+        int j = i-1;
+        while(j>=0 && a[j]<key){
+            a[j+1] = a[j];
+            j = j-1;
+        }
+        a[j+1] = key;
+    }
+    cout<<"Sorted array: ";
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+    cout<<"The second largest element is "<<a[1]<<endl;
+}
