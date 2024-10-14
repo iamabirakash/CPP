d947cc9f8568e15d29739426ebfdb84e0d124fd3
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2024-10-14 11:42:24 +0530

    Oct 14

---
+++ Class/INHERITANCE/MULTIPLE_INHERITANCE_PUBLIC.cpp
@@ -0,0 +1,46 @@
+// You are using GCC
+#include<iostream>
+using namespace std;
+class rectangle{
+    int length;
+    int breadth;
+    public:
+        int getl(){
+            cin>>length;
+            return length;
+        }
+        int getb(){
+            cin>>breadth;
+            return breadth;
+        }
+        int arear(){
+            int a = length*breadth;
+            return a;
+        }
+        
+};
+class circle{
+    int radius;
+    public:
+        void getr(){
+            cin>>radius;
+        }
+        float areac(){
+            float b = 3.14*radius*radius;
+            return b;
+        }
+};
+class area:public rectangle,public circle{
+    public:
+        void print(){
+            cout<<"Area of Rectangle: "<<arear()<<endl;
+            cout<<"Area of Circle: "<<areac();
+        }
+};
+int main(){
+    area a;
+    a.getl();
+    a.getb();
+    a.getr();
+    a.print();
+}
