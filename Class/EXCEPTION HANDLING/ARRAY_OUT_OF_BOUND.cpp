6df626fe92c1d0d0532922891577d6b12651ecb2
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2024-11-07 22:00:04 +0530

    Nov 7

---
+++ Class/EXCEPTION HANDLING/ARRAY_OUT_OF_BOUND.cpp
@@ -0,0 +1,16 @@
+int main(){
+    int a[5] = {1,2,3,4,5};
+    try{
+        int i = 0;
+        while(1){
+            cout<<a[i]<<endl;
+            i++;
+            if(i==5){
+                throw i;
+            }
+        }
+    } catch(int j){
+        cout<<"Array out of Bounds\n"<<j;
+    }
+    return 0;
+}
