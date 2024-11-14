9e09260a67d24237e810e7baa75aa2ddc74d3ff3
Abir Akash Bora <143283517+iamabirakash@users.noreply.github.com>
2024-11-14 14:37:14 +0530

    Nov 14

---
+++ Class/TEMPLATES/SWAPPING_NUMBER.cpp
@@ -0,0 +1,15 @@
+//WAP TO SWAP TWO VARIABLES OF SAME TYPE USING FUNCTION TEMPLATE
+#include<iostream>
+using namespace std;
+template<class T>
+T sswap(T a,T b){
+    T temp = a;
+    a = b;
+    b = temp;
+    cout<<a<<" "<<b;
+}
+int main(){
+    int x,y;
+    cin>>x>>y;
+    sswap<int>(x,y);
+}
